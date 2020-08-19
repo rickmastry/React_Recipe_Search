28699d316d545eb23535c58693438517e65753dc
jah rik <jahrik@hotmail.com>
2020-08-19 16:31:13 -0400

    master commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
 import './App.css';
+import Recipe from './components/Recipe';
+import Alert from './components/Alert';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+  const APP_ID = 'e6ec93d6';
+  const APP_KEY = 'ee6b8ff7f62b441c7fad563dfda2baca';
+  const PROXY = 'https://cors-anywhere.herokuapp.com/'
+  const url = `${PROXY}https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async() => {
+    if(query !== ""){
+      const result = await Axios.get(url);
+      if(!result.data.more){
+        return setAlert('Your search did not give any results')
+      }
+      setRecipes(result.data.hits)
+      console.log(result);
+      setAlert("");
+      setQuery("");
+    } else {
+      setAlert('Please fill the form');
+    }
+  
+  };
+  
+  const onChange = (e) => {
+     setQuery(e.target.value);
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mastry Media Recipe Search</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        {alert !== "" && <Alert alert={alert}/>}
+        <input type="text" placeholder="Search Food" autoComplete="off" onChange={onChange} value={query}/>
+        <input type="submit" value="search"/>
+      </form>
+      <div className="recipes">
+  {recipes !== [] && recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe}/>
+          )}
+      </div>
     </div>
   );
 }
